8bbba123c9e39f19313b4bdc851b54b4faeefb55
brunomecca <brunogmecca@gmail.com>
2017-10-15 22:33:50 -0200

    tentando

---
+++ tests/FirstClass.php
@@ -0,0 +1,18 @@
+<?php
+	
+	class FirstClass{
+
+		public function init() {
+	        
+	    }
+
+		public function soma($a, $b){
+			return $a+$b;
+		}
+
+		public function imgNow($imgSource){
+			return "<img src=". $imgSource . ">";
+		}
+	}
+
+?>
